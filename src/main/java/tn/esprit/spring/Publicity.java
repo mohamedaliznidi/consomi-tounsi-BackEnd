90ebe8492c4c8b7f417fa68780c8fef11ce9f6bf
jihenehassairi <jihene.hassairi@esprit.tn>
2021-03-08 22:02:54 +0100

    modifjihene

--- src/main/java/tn/esprit/spring/Publicity.java
+++ src/main/java/tn/esprit/spring/Publicity.java
@@ -1,9 +1,31 @@
-package tn.esprit.spring.entity;
-
+package tn.esprit.spring;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+@Entity
 public class Publicity {
-	int idPublicity;
-	Date datePublicity;
-
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name="PUBLICITY_ID")
+	private int id;
+	@Temporal (TemporalType.DATE)
+	private Date StartDateP;
+	@Enumerated(EnumType.ORDINAL)
+	private PublicityCategory publicitycategory;
+	
+	public enum PublicityCategory {
+		Balise1 , Balise2, Balise3,Balise4 , Balise5, BaliseForum, BaliseHome1,BaliseHome2,BaliseHome3,
+		BaliseJackpot, BaliseEvent,BaliseDonation,
+		
+	}
+	
 }
+
