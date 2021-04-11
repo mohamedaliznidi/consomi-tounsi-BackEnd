05b1a8d5770e6c2257556e225aaeb08d8b12289a
jihenehassairi <jihene.hassairi@esprit.tn>
2021-04-11 15:51:35 +0200

    okok

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -1,80 +1,77 @@
 package tn.esprit.spring.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue; 
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 
 @Entity
-
-
-@Inheritance( strategy=InheritanceType.TABLE_PER_CLASS)
+@Inheritance( strategy=InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="USER_TYPE")
 public abstract class User implements Serializable {
+
+	protected static final long serialVersionUID = 1L;
 	
-	
-	/**
-	 * @param id
-	 * @param name
-	 * @param email
-	 * @param password
-	 * @param phone_number
-	 */
-	public User(int id, String name, String email, String password, int phone_number) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.phone_number = phone_number;
-	}
-
-	public User(String name, String email, String password, int phone_number) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.phone_number = phone_number;
-	}
-
-
-	public User() {
-		super();
-	}
-
-	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
 	@Column(name="USER_ID")
+	protected int id; // Clé primaire
+
+	@Column(name="USER_NAME",unique = true, nullable = false)
+	@NotBlank
+    @Size(max = 15)
+	protected String user_Name;
+
+	@NotBlank
+    @Size(max = 40)
+    @Email
+	@Column(name="USER_EMAIL",unique = true, nullable = false)
+	protected String email;
+
 	
-	private int id; // Clé primaire
+	@NotBlank
+    @Size(max = 100)
+    @JsonIgnore
+	@Column(name="USER_PW",unique = true, nullable = false)
+	protected String password;
 
-	@Column(name="USER_NAME")
+	@Column(name="USER_PHONE", nullable = false)
+	protected int phone_number;
+
+	@Column(name="ROLE")
+	protected String Role;
 	
-	private String name;
+	@Column(name="USER_FIRST_NAME",  nullable = false)
+	protected String first_Name;
 
-	@Column(name="USER_EMAIL")
+	@Column(name="USER_LAST_NAME", nullable = false)
+	protected String last_Name;
+
+	@Column(name="CREATED_AT", nullable = false)
+	@CreatedDate
+    protected Instant createdAt = Instant.now();
+
+	@Column(name="UPDATED_AT", nullable = false)
+    @LastModifiedDate
+    protected Instant updatedAt = Instant.now();
+
+	@Column(name="ACTIVE", nullable = false)
+    protected boolean active;
+
 	
-	private String email;
-
-	@Column(name="USER_PW")
-
-	private String password;
-
-	@Column(name="USER_PHONE")
-
-	private int phone_number;
-
+	
 	//Getters&Setters
+	
 	
 	public int getId() {
 		return id;
@@ -86,16 +83,16 @@ public abstract class User implements Serializable {
 	}
 
 	
-	public String getName() {
-		return name;
+	public String getUser_Name() {
+		return user_Name;
 	}
 
 	
-	public void setName(String name) {
-		this.name = name;
+	public void setUser_Name(String user_Name) {
+		this.user_Name = user_Name;
 	}
 
-	
+
 	public String getEmail() {
 		return email;
 	}
@@ -105,16 +102,16 @@ public abstract class User implements Serializable {
 		this.email = email;
 	}
 
-	
 	public String getPassword() {
 		return password;
 	}
 
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	
 	public int getPhone_number() {
 		return phone_number;
 	}
@@ -124,8 +121,116 @@ public abstract class User implements Serializable {
 		this.phone_number = phone_number;
 	}
 
+	
+	public String getRole() {
+		return Role;
+	}
+
+	
+	public void setRole(String role) {
+		Role = role;
+	}
+
+	
+	public String getFirst_Name() {
+		return first_Name;
+	}
+
+	
+	public void setFirst_Name(String first_Name) {
+		this.first_Name = first_Name;
+	}
+
+	
+	public String getLast_Name() {
+		return last_Name;
+	}
+
+	
+	public void setLast_Name(String last_Name) {
+		this.last_Name = last_Name;
+	}
+
+	
+	/*public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}*/
+	
+	public boolean isActive() {
+		return active;
+	}
+
+	public boolean getActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	
+	//Constractors
+	
+	
+	public User(User user) {
+		this.id = user.id;
+		this.user_Name = user.user_Name;
+		this.email = user.email;
+		this.password = user.password;
+		this.phone_number = user.phone_number;
+		this.Role = user.Role;
+		this.first_Name = user.first_Name;
+		this.last_Name = user.last_Name;
+		//this.createdAt = user.createdAt;
+		//this.updatedAt = user.updatedAt;
+		this.active = user.active;
+	}
+
+
+	public User(int id, @NotBlank @Size(max = 15) String user_Name, @NotBlank @Size(max = 40) @Email String email,
+			@NotBlank @Size(max = 100) String password, int phone_number, String Role, String first_Name,
+			String last_Name, Instant createdAt, Instant updatedAt, boolean active) {
+		this.id = id;
+		this.user_Name = user_Name;
+		this.email = email;
+		this.password = password;
+		this.phone_number = phone_number;
+		this.Role = Role;
+		this.first_Name = first_Name;
+		this.last_Name = last_Name;
+		//this.createdAt = createdAt;
+		//this.updatedAt = updatedAt;
+		this.active = active;
+	}
+
+
+	public User(@NotBlank @Size(max = 15) String user_Name, @NotBlank @Size(max = 40) @Email String email,
+			@NotBlank @Size(max = 100) String password) {
+		this.user_Name = user_Name;
+		this.email = email;
+		this.password = password;
+		this.active = true;
+	}
+
+
 }
