c3155a10da81ad898753e221043df9f56c8f2dc4
mohamedaliznidi <57480839+mohamedaliznidi@users.noreply.github.com>
2021-05-15 20:26:43 +0200

    Update script.js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,41 +1,20 @@
 'use strict'
 
-//import jwt_decode from "jwt-decode";
-let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYWxpem5haWRpIiwiYXV0aG9yaXRpZXMiOlsiQURNSU4iXSwiaWF0IjoxNjIxMDk3OTc2LCJleHAiOjE2MjExODQzNzZ9.dqYlu_HcxreZMFvCKd6kDv5osFK84spXKNLFEMdpMUbt5v9GZnRnSgzvxKelbTM-cuOg-RYQ_m0ysoNsDTw8eg" 
-//localStorage.getItem("token");
-console.log(token);
-let decoded = jwt_decode(token);
-console.log(decoded);
-console.log("heloo");
-
 let stompClient
 let username
 
 function parseURLParams(url) {
-    var queryStart = url.indexOf("0") + 1,
-        queryEnd   = url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
-
-    if (query === url || query === "") return;
-
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=", 2);
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
-
-        if (!parms.hasOwnProperty(n)) parms[n] = [];
-        parms[n].push(nv.length === 2 ? v : null);
-    }
-    return parms;
+  var queryStart = url.indexOf("#") + 1,
+    queryEnd = url.length + 1,
+    query = url.slice(queryStart, queryEnd - 1) ;
+  return query;
 }
 
 window.onload = connect;
 
 function connect(event) {
     console.log(parseURLParams(window.location.href));
-    username =  decoded.sub;//document.querySelector('#username').value.trim()
+    username = parseURLParams(window.location.href); 
 
     if (username) {
         const login = document.querySelector('#login')
